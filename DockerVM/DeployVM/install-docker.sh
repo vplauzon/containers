8bc97dbec4f8b613cb9c33fd39a08c39fbfbf0c8
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-04-01 17:39:41 -0400

    .

--- DockerVM/DeployVM/install-docker.sh
+++ DockerVM/DeployVM/install-docker.sh
@@ -1,1 +1,24 @@
-﻿
+﻿###	This file should be run in SUDO mode
+
+#	Install tools
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+#	Setup stable repo
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+#	Enable edge & test repo
+yum-config-manager --enable docker-ce-edge
+yum-config-manager --enable docker-ce-test
+
+#	Install latest version of Docker CE
+yum install docker-ce -y
+
+#	Start Docker
+systemctl start docker
+
+#	Run Hello World
+docker run hello-world
