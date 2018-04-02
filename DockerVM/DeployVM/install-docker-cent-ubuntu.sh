37a928d68dc63b1d54b1f7fe00540465accd05ca
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-04-02 06:45:42 -0400

    .

---
+++ DockerVM/DeployVM/install-docker-cent-ubuntu.sh
@@ -0,0 +1,29 @@
+﻿#!/bin/bash
+
+###	This file should be run in SUDO mode
+
+### The script file needs to be executable, i.e.
+#	chmod +x script.sh
+
+#	Install tools
+yum install -y yum-utils \
+  device-mapper-persistent-data \
+  lvm2
+
+#	Setup stable repo
+yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+#	Enable edge & test repo
+yum-config-manager --enable docker-ce-edge
+yum-config-manager --enable docker-ce-test
+
+#	Install latest version of Docker CE
+yum install docker-ce -y
+
+#	Start Docker
+systemctl start docker
+
+#	Run Hello World
+docker run hello-world
