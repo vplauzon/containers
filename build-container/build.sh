c2296413d79c99fa29c05219229db146f6cac4d0
Vincent-Philippe Lauzon <vplauzon@MININT-BK6A5VR.northamerica.corp.microsoft.com>
2018-04-04 14:08:02 -0400

    .

--- build-container/build.sh
+++ build-container/build.sh
@@ -1,6 +1,4 @@
-﻿#!/bin/bash
-
-###	This file should be run in SUDO mode
+﻿###	This file should be run in SUDO mode
 
 # Clone a git repo
 git clone https://github.com/vplauzon/containers.git
@@ -8,5 +6,4 @@ git clone https://github.com/vplauzon/containers.git
 # Go to the folder inside the repo containing the Dockerfile
 cd containers/get-started-no-redis/
 
-docker build -t mycontainer
-
+docker build -t mycontainer .
