ba25340afd3ed9c8f525381f5ae2b1adb955eb88
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-04-05 18:03:21 -0400

    Add bootstrap

---
+++ DockerVM/DeployVM/bootstrap-cent-os.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+###	This file should be run in SUDO mode
+
+### The script file needs to be executable, i.e.
+#	chmod +x script.sh
+
+#	Install Docker
+sh install-docker-cent-os.sh
+
+#	Install Azure CLI
+sh install-azure-cli-cent-os.sh
---
+++ DockerVM/DeployVM/bootstrap-ubuntu.sh
@@ -0,0 +1,12 @@
+﻿#!/bin/bash
+
+###	This file should be run in SUDO mode
+
+### The script file needs to be executable, i.e.
+#	chmod +x script.sh
+
+#	Install Docker
+sh install-docker-ubuntu.sh
+
+#	Install Azure CLI
+sh install-azure-cli-ubuntu.sh
