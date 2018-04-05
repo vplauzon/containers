191387b2b3b1b7dfa401ef14304dbdeaa5d3de31
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-04-05 19:06:16 -0400

    .

--- DockerVM/DeployVM/install-azure-cli-cent-os.sh
+++ DockerVM/DeployVM/install-azure-cli-cent-os.sh
@@ -12,4 +12,4 @@ rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sh -c 'echo -e "[azure-cli]\nname=Azure CLI\nbaseurl=https://packages.microsoft.com/yumrepos/azure-cli\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/azure-cli.repo'
 
 #	Install with the yum install command
-yum install azure-cli
+yum install azure-cli -y
