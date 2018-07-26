6b6f3150cdacb552d6849d0cb8987e280ba2118e
vplauzon <vincent_lauzon@yahoo.com.au>
2018-07-26 07:33:47 -0400

    fix digital signature

--- DockerVM/DeployVM/install-azure-cli-ubuntu.sh
+++ DockerVM/DeployVM/install-azure-cli-ubuntu.sh
@@ -8,10 +8,11 @@
 #	Modify sources list
 AZ_REPO=$(lsb_release -cs)
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $AZ_REPO main" | \
-     sudo tee /etc/apt/sources.list.d/azure-cli.list
+    sudo tee /etc/apt/sources.list.d/azure-cli.list
+
+#	Get Microsoft signing key:
+curl -L https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 
 #	Install tools
-apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
 apt-get install apt-transport-https
 apt-get update && sudo apt-get install azure-cli
-
